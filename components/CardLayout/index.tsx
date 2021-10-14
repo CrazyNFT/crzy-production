bb11b51ab5d95890575448f341798a091edea9aa
Shivansh Goel <shivanshgoel21@gmail.com>
2021-10-14 11:49:34 +0530

    CardLayout/index.tsx: integrate retrieve all NFT

--- components/CardLayout/index.tsx
+++ components/CardLayout/index.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NftCard from "@/components/NftCard";
 import { nftData } from "@/components/tempdata/samplenfts.jsx";
 import Grid from "@material-ui/core/Grid";
 
+import NFT from '../../services/models/nft'
+
 export default function CardLayout() {
+
+  const [nftData1, setNftData1] = useState([])
+
+  useEffect(() => {
+    const getNFT = async () => {
+      try {
+        let nft = new NFT()
+        let data = await nft.getAll()
+        console.log('NFT Data ', data)
+        if (data) {
+          setNftData1(data)
+        }
+      } catch (err) {
+        alert('Error! ' + err)
+      }
+    };
+  
+    getNFT()
+  }, []);
+
+
   return (
     <Grid container spacing={2}>
       {nftData.map((data, i) => {
